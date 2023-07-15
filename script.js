5189bb645afb0be959605f3841b068f57a6b8d17
Diana Castaño <dianaczapata@gmail.com>
2023-07-15 11:27:18 -0500

    hamburger menu working

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const hamburger= document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
 let line1Bar= document.querySelector(".line1__bars--menu");
 let line2Bar= document.querySelector(".line2__bars--menu");
 let line3Bar= document.querySelector(".line3__bars--menu");
+const listElements = document.querySelectorAll(".menu__item-show")
 
 hamburger.addEventListener("click", animateMenu);
 
@@ -11,10 +13,47 @@ function animateMenu(){
   line3Bar.classList.toggle("activateline3__bars--menu");
 }
 
-
-
-
 function toggleMenu() {
-  let menu = document.querySelector(".menu");
-  menu.style.display = (menu.style.display === "block") ? "none" : "block";
+  menu.classList.toggle("menu__links-show")
+}
+
+function addClick(){
+  listElements.forEach(element => {
+    element.addEventListener("click", () =>{
+      let subMenu= element.children[1]
+      let height=0
+      element.classList.toggle("menu__item-active")
+
+      if(subMenu.clientHeight === 0){
+        height = subMenu.scrollHeight
+      }
+
+      subMenu.style.height= `${height}px`
+
+    })
+  })
+}
+
+function deleteStyleHeight(){
+  listElements.forEach(element => {
+    if(element.children[1].getAttribute("style")){
+      element.children[1].removeAttribute("style")
+      element.classList.remove("menu__item-active")
+    }
+  })
+}
+
+window.addEventListener("resize", () =>{
+  if(window.innerWidth > 800){
+    deleteStyleHeight()
+    if(menu.classList.contains("menu__links-show")){
+      menu.classList.remove("menu__links-show")
+    }
+  }else{
+    addClick()
+  }
+})
+
+if(window.innerWidth <=800){
+  addClick()
 }
